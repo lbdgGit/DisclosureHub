1e5ada6025305b31c3d4a223ab170a9f0bc6bf12
lbdgGit <lbdgfrance@gmail.com>
2026-07-30 13:15:06 +0200

    Update page.tsx

--- disclosure-hub-starter/app/maturity/page.tsx
+++ disclosure-hub-starter/app/maturity/page.tsx
@@ -1,1 +1,14 @@
+import DisclosureMaturity from "@/components/DisclosureMaturity";
+import military from "@/content/tracks/military.json";
+import government from "@/content/tracks/government.json";
+import legislative from "@/content/tracks/legislative.json";
+import scientific from "@/content/tracks/scientific.json";
+import financial from "@/content/tracks/financial.json";
+import media from "@/content/tracks/media.json";
+import international from "@/content/tracks/international.json";
 
+const tracks = [military, government, legislative, scientific, financial, media, international];
+
+export default function Page() {
+  return <DisclosureMaturity tracks={tracks} />;
+}
