39296f8344cce6d19e7542f138e64610e4ba2bc3
lbdgGit <lbdgfrance@gmail.com>
2026-07-31 13:26:39 +0200

    Update InstitutionalSignals.tsx

--- disclosure-hub-starter/components/InstitutionalSignals.tsx
+++ disclosure-hub-starter/components/InstitutionalSignals.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { DVIBadge } from '@/components/DVIBadge';
-import { SIGNALS, CATEGORY_CONFIG, STRENGTH_CONFIG } from '@/data/signals';
 import { SIGNALS, CATEGORY_CONFIG, WEIGHT_CONFIG, getWeightTier} from '@/data/signals';
 const DVI_VALUE = 6.5;
 const DVI_LEVEL = 'READINESS';
